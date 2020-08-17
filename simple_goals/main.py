PNAS bug fixes + put all the PNAS implementation in a single file. Added 2 goal units (tea 6 coffee) to PNAS implementation, +code to produce RDMs and MDSs. Not clean code, but this is a one-off side-project.

simple_goals/main.py
@@ -2,10 +2,18 @@
 import teacoffeeenv as tce
 import scripts
 import neuralnet as nn
+import utils
+import pnas2018
 
 #scripts.train_supervised(nn.NeuralNet(), 10000)
-#scripts.make_rdm_and_mds('noise_test2')
+pnas2018.make_rdm_multiple('pnasrdm', 100, with_goals=False)
+#pnas2018.make_rdm_and_mds_pnas('pnas2')
 
-for i in range(100):
-    model = scripts.train_pnas()
-    scripts.accuracy_test_pnas(model)
+
+"""
+for i in range(7):
+    model = pnas2018.train_pnas()
+    pnas2018.accuracy_test_pnas(model)
+    utils.save_object("pnasrdmnogoals", model)
+
+"""

simple_goals/neuralnet.py
@@ -59,10 +59,10 @@ class NeuralNet(object):
                         self.h_action_wta, self.h_goal1_wta, self.h_goal2_wta]
 
     def feedforward(self, observation):
-        # Not sure if thatś necessary? In theory the winner take all has no gradient anyway.
         network_input = tf.concat([self.context, observation], 1)
         for inputs in [self.action, self.goal1, self.goal2]:
             if inputs is not None:
+                # Not sure if thatś necessary? In theory the winner take all has no gradient anyway.
                 inputs = tf.stop_gradient(inputs)
                 network_input = tf.concat([network_input, inputs], 1)
         hidden_activation = self.dense_sigmoid(network_input, self.hidden_layer)

simple_goals/scripts.py
@@ -52,7 +52,7 @@ def train_supervised(model, num_episodes):
         with tf.GradientTape() as tape:
             # Initialize context with random/uniform values.
             #model.context = np.zeros((1, model.size_hidden))
-            model.context = np.random.uniform(0.01, 0.99, (1, model.size_hidden))
+            model.context = np.float32(np.random.uniform(0.01, 0.99, (1, model.size_hidden)))
             targets_onehot = [[], [], []]  # actions, goal1s, goal2s
             for i, target in enumerate(targets):
                 # Set up the input to be the correct actions and goals
@@ -110,6 +110,8 @@ def get_model_hidden_activations(model):
             hidden.append(model.context.numpy().flatten())
             env.do_action(target[0])
     return hidden
+
+
 
 
 def run_models_with_noise(models, noise):
@@ -186,6 +188,7 @@ def make_rdm_and_mds(name):
     analysis.plot_mds_points(mdsy, range(len(mdsy)), labels=labels)
 
 
+
 def plot_effects_of_noise(name):
     models = utils.load_objects(name, 10)
     action_errors = []
@@ -197,109 +200,4 @@ def plot_effects_of_noise(name):
     for i in range(len(action_errors)):
         plt.plot(range(len(action_errors[i])), action_errors[i], colors[i] + 'x-')
     plt.show()
-
-
-def train_pnas():
-    model = nn.NeuralNet(size_hidden=15, size_observation=7, size_action=8, size_goal1=0, size_goal2=0)
-    num_episodes = 10000
-    model.learning_rate = .5
-    model.L2_regularization = 0.
-
-    rng_avg_loss = 0.
-    rng_avg_actions = 0.
-    rng_avg_goals = 0.
-
-    all_inputs = ["start", "coffee", "milk", "water", "stir", "tea", "sugar"]
-    all_outputs = ["coffee", "milk", "water", "stir", "sugar", "tea", "serve_coffee", "serve_tea"]
-    seq1 = ["start", "coffee", "milk", "stir", "water", "stir", "serve_coffee"]
-    seq2 = ["start", "coffee", "water", "stir", "milk", "stir", "serve_coffee"]
-    seq3 = ["start", "tea", "sugar", "stir", "water", "stir", "serve_tea"]
-    seq4 = ["start", "tea", "water", "stir", "sugar", "stir", "serve_tea"]
-    seqs = [seq1, seq2, seq3, seq4]
-
-    for episode in range(num_episodes):
-        sequence = seqs[np.random.randint(len(seqs))]
-        inputs = utils.liststr_to_onehot(sequence[:-1], all_inputs)
-        targets = utils.liststr_to_onehot(sequence[1:], all_outputs)
-        model.action = np.zeros((1, model.size_action), dtype=np.float32)
-        # run the network
-        with tf.GradientTape() as tape:
-            # Initialize context with random/uniform values.
-            model.context = np.zeros((1, model.size_hidden), dtype=np.float32) #np.float32(np.random.uniform(0.01, 0.99, (1, model.size_hidden)))
-            for i in range(len(targets)):
-                model.action = np.zeros((1, model.size_action), dtype=np.float32)
-                observation = inputs[i].reshape(1, -1)
-                model.feedforward(observation)
-
-            # Get some statistics about what was correct and what wasn't
-            tchoices = np.array(model.h_action_wta).reshape((-1, len(targets[0])))
-            ratios = evaluate([tchoices], [targets])
-            # Train model, record loss.
-            loss = model.train(targets, None, None, tape)
-
-        # Monitor progress using rolling averages.
-        speed = 2. / (
-                    episode + 2) if episode < 1000 else 0.001  # enables more useful evaluations for early trials
-        rng_avg_loss = utils.rolling_avg(rng_avg_loss, loss, speed)
-        rng_avg_actions = utils.rolling_avg(rng_avg_actions, ratios[0], speed)
-        rng_avg_goals = utils.rolling_avg(rng_avg_goals, ratios[0] == 1,
-                                          speed)  # whole action sequence correct ?
-        # Display on the console at regular intervals
-        if (episode < 1000 and episode in [3 ** n for n in range(50)]) or episode % 1000 == 0 \
-                or episode + 1 == num_episodes:
-            print(
-                "{0}: avg loss={1}, \tactions={2}, \tfull_sequence={3}".format(
-                    episode, rng_avg_loss, rng_avg_actions, rng_avg_goals))
-    return model
-
-def make_pnas_models(num_models):
-    models = []
-    for i in range(num_models):
-        models.append(train_pnas())
-        accuracy_test_pnas(models[-1])
-
-def accuracy_test_pnas(model):
-    all_inputs = ["start", "coffee", "milk", "water", "stir", "tea", "sugar"]
-    all_outputs = ["coffee", "milk", "water", "stir", "sugar", "tea", "serve_coffee", "serve_tea"]
-    seq1 = ["start", "coffee", "milk", "stir", "water", "stir", "serve_coffee"]
-    seq2 = ["start", "coffee", "water", "stir", "milk", "stir", "serve_coffee"]
-    seq3 = ["start", "tea", "sugar", "stir", "water", "stir", "serve_tea"]
-    seq4 = ["start", "tea", "water", "stir", "sugar", "stir", "serve_tea"]
-    seqs = [seq1, seq2, seq3, seq4]
-
-    all_choices = []
-    for sequence in seqs:
-        seq_choices = []
-        all_choices.append(seq_choices)
-        inputs = utils.liststr_to_onehot(sequence[:-1], all_inputs)
-        targets = utils.liststr_to_onehot(sequence[1:], all_outputs)
-        model.action = np.zeros((1, model.size_action), dtype=np.float32)
-        # run the network
-        with tf.GradientTape() as tape:
-            # Initialize context with random/uniform values.
-            # model.context = np.zeros((1, model.size_hidden))
-            model.context = np.zeros((1, model.size_hidden), dtype=np.float32) #np.float32(np.random.uniform(0.01, 0.99, (1, model.size_hidden)))
-            # Reset the previous action
-            model.action = np.zeros((1, model.size_action), dtype=np.float32)
-            #targets_onehot = [[], [], []]  # actions, goal1s, goal2s
-            for i in range(len(targets)):
-                observation = inputs[i].reshape(1, -1)
-                model.feedforward(observation)
-
-                #env.do_action(targets[i])  # Transition the MDP according to the *target* action, not the chosen action!
-
-            # Get some statistics about what was correct and what wasn't
-            choice = np.array(model.h_action_wta).reshape((-1, len(targets[0])))
-            model.h_action_wta.clear()
-            seq_choices.append(choice)
-
-    # Now evaluate accuracy:
-    accuracy_totals = np.zeros((len(seq1)-1))
-    for i in range(len(all_choices)):
-        targets = utils.liststr_to_onehot(seqs[i][1:], all_outputs)
-        for j in range(len(targets)):
-            if (all_choices[i][0][j] == targets[j]).all():
-                accuracy_totals[j] += 1
-    accuracy_totals /= 4
-    print(accuracy_totals)
 